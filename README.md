d360b48130b1284db91ba2f091ea5a082fe4993b
finkord <devfinkord@gmail.com>
2025-11-09 00:45:16 +0200

    added model product and category

--- README.md
+++ README.md
@@ -1,31 +1,4 @@
 #
 
-![Local application run](screenshots/flask_shell.png)
-*Створення бази даних через фласк оболонку*
-
-![Local application run](screenshots/migration_init.png)
-*Ініціалізація міграції і бази даних*
-
-![Local application run](screenshots/migrate_is_active_author.png)
-*Міграція (добавлено два нових поля)*
-
-![Local application run](screenshots/sqllite_posts.png)
-*База даних після міграції*
-
-![Local application run](screenshots/tests3.png)
-*Успішне проходження тестів*
-
-![Local application run](screenshots/flash_create_post.png)
-*Повідомлення про створення поста і сторінка всіх постів*
-
-![Local application run](screenshots/detail_page.png)
-*сторінка одного поста*
-
-![Local application run](screenshots/delete_page.png)
-*сторінка підтвердження видалення*
-
-![Local application run](screenshots/flash_delete_post.png)
-*повідомлення про видалення*
-
-![Local application run](screenshots/admin.png)
-*пост від адміна*
+![Local application run](screenshots/sqlinfo.png)
+*flask db history і поля таблиці products*
--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from .config import config_map
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
@@ -14,7 +15,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention={
+        "ix": 'ix_%(column_0_label)s',
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    })
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
@@ -68,6 +75,12 @@ def create_app(config_name: str = os.environ.get("FLASK_CONFIG", "dev")) -> Flas
         app.register_blueprint(posts_blueprint)
 
         from .posts import models
+
+        # Import and register the 'products' Blueprint
+        from .products import products_bp as products_blueprint
+        app.register_blueprint(products_blueprint)
+
+        from .products import models
 
         # Print routes only when testing
         if config_name == "test":
